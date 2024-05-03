6858b0eaddc1f79e9adc8ee472c0e03d4cd49a50
MatijaVukadinovic <matija.vukadinovic@gmail.com>
2024-05-03 20:24:53 +0200

    Declare reference variables for HTML content

---
+++ script.js
@@ -0,0 +1,4 @@
+let number = document.getElementById("numberDisplay");
+let decreaseButton =  document.getElementById("decreaseButton");
+let increaseButton =  document.getElementById("increaseButton");
+let resetButton = document.getElementById("resetButton");
