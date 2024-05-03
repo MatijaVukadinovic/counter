43d8fc24772f3e3c3f97204696a87fe34da0ea8b
MatijaVukadinovic <matija.vukadinovic@gmail.com>
2024-05-03 20:42:59 +0200

    Create coloring logic in script.js

--- script.js
+++ script.js
@@ -5,12 +5,34 @@ let resetButton = document.getElementById("resetButton");
 
 increaseButton.addEventListener("click", function() {
     number.innerHTML = Number(number.innerHTML) + 1;
+    
+    if (Number(number.innerHTML) == 0) {
+        number.classList.remove("redText");
+        number.classList.remove("greenText");
+    }
+
+    if (Number(number.innerHTML) > 0) {
+        number.classList.remove("redText");
+        number.classList.add("greenText");
+    }
 });
 
 decreaseButton.addEventListener("click", function() {
     number.innerHTML = Number(number.innerHTML) - 1;
+
+    if (Number(number.innerHTML) == 0) {
+        number.classList.remove("redText");
+        number.classList.remove("greenText");
+    }
+
+    if (Number(number.innerHTML) < 0) {
+        number.classList.remove("greenText");
+        number.classList.add("redText");
+    }
 });
 
 resetButton.addEventListener("click", function() {
     number.innerHTML = 0;
+    number.classList.remove("greenText");
+    number.classList.remove("redText");
 })
