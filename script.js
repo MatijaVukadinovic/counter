04854982d2c12f5ce72b2fdca2462d247dba4c1b
MatijaVukadinovic <matija.vukadinovic@gmail.com>
2024-05-03 20:35:05 +0200

    Create button logic

--- script.js
+++ script.js
@@ -2,3 +2,15 @@ let number = document.getElementById("numberDisplay");
 let decreaseButton =  document.getElementById("decreaseButton");
 let increaseButton =  document.getElementById("increaseButton");
 let resetButton = document.getElementById("resetButton");
+
+increaseButton.addEventListener("click", function() {
+    number.innerHTML = Number(number.innerHTML) + 1;
+});
+
+decreaseButton.addEventListener("click", function() {
+    number.innerHTML = Number(number.innerHTML) - 1;
+});
+
+resetButton.addEventListener("click", function() {
+    number.innerHTML = 0;
+})
